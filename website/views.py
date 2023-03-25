f02f8343455b6a623a4774dbe524047a695cde69
Youssef <youssef3698@gmail.com>
2023-03-25 10:34:44 +0100

    Registration

--- website/views.py
+++ website/views.py
@@ -25,3 +25,6 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out...")
     return redirect('home')
+
+def register_user(request):
+    return render(request, "register.html", {})
